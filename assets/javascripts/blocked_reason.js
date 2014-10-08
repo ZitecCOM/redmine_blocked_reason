bc13c1ff920d0a54f6bc20d7a11fbcd17354c3f9
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-08 12:35:07 +0300

    Radio button list and popup window for the block form

--- assets/javascripts/blocked_reason.js
+++ assets/javascripts/blocked_reason.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
-    $('#block_issue_button').bind('click', function() {
-      $('#blocked_reason_form').show();
+    block_issue_button = $('#block_issue_button');
+    blocked_reason_form = $('#blocked_reason_form');
+    block_form_hide_button = $('#block_form_hide_button');
+    new_blocked_reason = $('#new_blocked_reason');
+
+    block_issue_button.bind('click', function() {
+      blocked_reason_form.show();
+      blocked_reason_form.attr('top',block_issue_button.position().top);
+      blocked_reason_form.attr('left',block_issue_button.position().left + block_issue_button.width());
     });
-    $('#new_blocked_reason').bind('submit', function() {
-      var items = ['blocked_reason_blocked_reason_type_id', 'blocked_reason_comment']
-      for(var i in items) {
-        var element = $('#' + items[i]);
-        if (element.val() == ''){ 
-          element.attr('style', 'border-color:red;')
-          return false;
-        }
-      }
+    block_form_hide_button.bind('click', function() {
+      blocked_reason_form.hide();
+    });
+    new_blocked_reason.bind('submit', function() {
       return true;
     });
+
     $('.tip').tipr({
           // 'speed': 300,
           // 'mode': 'top'
