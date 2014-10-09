3fa8c11381dee6bfb187ea01112809f91cce500e
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-09 18:13:44 +0300

    Assets rendering handled by layout header hook

--- lib/redmine_blocked_reason/hooks/issue_view_hook.rb
+++ lib/redmine_blocked_reason/hooks/issue_view_hook.rb
@@ -10,6 +10,15 @@ module RedmineBlockedReason
         end
       end
 
+      def view_layouts_base_html_head(context)
+        project, controller = context[:project], context[:controller]
+        if project.module_enabled? :blocked_reason
+          controller.render_to_string partial: 'blocked_reason/header_assets'
+        else
+          ''
+        end
+      end
+
       def view_issues_rows(context)
         unless context[:issue].blocked_reason.nil?
           context[:rows].left l(:blocked_reason), "<span data-tip='#{h(context[:issue].blocked_reason.comment)}' class='tip blocked_reason_comment'>#{h(context[:issue].blocked_reason.type_name)}</span>".html_safe, :class => 'blocked_reason'
