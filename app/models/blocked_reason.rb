7b905671d5f4c5f5a39562832613a772c9c33e7d
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-10 17:21:53 +0300

    Interaction with the activity stream

--- app/models/blocked_reason.rb
+++ app/models/blocked_reason.rb
@@ -3,4 +3,15 @@ class BlockedReason < ActiveRecord::Base
   belongs_to :blocked_reason_type
   belongs_to :issue
   delegate :name, to: :blocked_reason_type, prefix: :type
+
+  acts_as_event :datetime => :updated_at,
+    :description => :name,
+    :title => Proc.new { |o| "##{o.id} - #{o.name}" },
+    :url => Proc.new { |o| { :controller => 'issues', :action => 'show', :id => o.issue.id,
+      :project_id => o.project }
+    }
+
+  acts_as_activity_provider find_options: { include: :project },
+    type: 'blocked_reason',
+    timestamp: :updated_at
 end
