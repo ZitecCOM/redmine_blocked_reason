3bf934abb4ccefa7c4c5fb1eec1dbba57d1b661c
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-16 13:06:14 +0300

    Progress on filtering by blocked reason, currently displays all isues that are blocked

--- lib/redmine_blocked_reason/patches/blocked_reason_query_patch.rb
+++ lib/redmine_blocked_reason/patches/blocked_reason_query_patch.rb
@@ -5,6 +5,7 @@ module RedmineBlockedReason
       base.class_eval do
         unloadable
         alias_method_chain :available_filters, :blocked_reasons
+        alias_method_chain :sql_for_field, :blocked_reasons
       end
     end
     module InstanceMethods
@@ -18,6 +19,21 @@ module RedmineBlockedReason
             :order => @available_filters.size + 1}
         }
         @available_filters.merge!(blocked_reason_filters)
+      end
+
+      def sql_for_field_with_blocked_reasons(field, operator, value, db_table, db_field, is_custom_filter=false)
+        if field == 'blocked_reason'
+          return <<-SQL
+            issues.id IN (
+              SELECT DISTINCT i.id FROM issues i
+                INNER JOIN blocked_reasons br ON br.issue_id = i.id
+                INNER JOIN blocked_reason_types brt ON br.blocked_reason_type_id = brt.id
+              WHERE br.active = true AND br.unblocker = false AND brt.removed = false
+            )
+          SQL
+        else
+          return sql_for_field_without_blocked_reasons(field, operator, value, db_table, db_field, is_custom_filter)
+        end
       end
     end
   end
