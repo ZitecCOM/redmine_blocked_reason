45065cc14d7e9edbf777c1f3b84bab10f8b978a1
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-20 13:16:48 +0300

    Managed to implement tags with links, it really worked WOW

--- lib/redmine_blocked_reason/patches/blocked_reason_query_helper_patch.rb
+++ lib/redmine_blocked_reason/patches/blocked_reason_query_helper_patch.rb
@@ -17,7 +17,7 @@ module RedmineBlockedReason
 
       module InstanceMethods
         def column_value_with_blocked_reason_tag(column, issue, value)
-          tag = column.name.eql?(:subject) ? issue.blocked_reason_tag : ''
+          tag = column.name.eql?(:subject) ? render('blocked_reason/blocked_reason_tag', issue: issue) : ''
           column_value_without_blocked_reason_tag(column, issue, value) + tag
         end
       end
