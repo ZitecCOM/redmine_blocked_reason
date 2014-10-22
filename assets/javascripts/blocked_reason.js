181e1660dbcc84f6877330b74aa2a3e2df6081c1
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-22 16:16:56 +0300

    Some validations for the blocked reason form

--- assets/javascripts/blocked_reason.js
+++ assets/javascripts/blocked_reason.js
@@ -1,18 +1,40 @@
-$(document).ready(function() {
-  block_issue_button = $('#block_issue_button');
-  blocked_reason_form = $('#blocked_reason_form');
-  block_form_hide_button = $('#block_form_hide_button');
-  new_blocked_reason = $('#new_blocked_reason');
+$(document).on('click', function(event) {
+  if (!$(event.target).closest('#blocked_reason_form').length && !$(event.target).closest('#block_issue_button').length) {
+    $('#blocked_reason_form').hide();
+  }
+});
 
+$(document).ready(function() {
+  var block_issue_button = $('#block_issue_button');
+  var blocked_reason_form = $('#blocked_reason_form');
+  var block_form_hide_button = $('#block_form_hide_button');
+  var new_blocked_reason_button = $('#new_blocked_reason');
+  var blocked_reason_comment = $('#blocked_reason_comment');
+  var reason_label = $('#reason_label');
+
+  blocked_reason_comment.val('');
   block_issue_button.bind('click', function() {
+    var position = block_issue_button.position();
+    var pos_top = position.top + block_issue_button.height();
+    var pos_left = position.left - blocked_reason_form.width() + block_issue_button.width() / 2;
+    blocked_reason_form.css({
+      top: pos_top,
+      left: pos_left
+    });
     blocked_reason_form.show();
-    blocked_reason_form.attr('top',block_issue_button.position().top);
-    blocked_reason_form.attr('left',block_issue_button.position().left + block_issue_button.width());
   });
   block_form_hide_button.bind('click', function() {
     blocked_reason_form.hide();
   });
-  new_blocked_reason.bind('submit', function() {
+  new_blocked_reason_button.bind('click', function() {
+    if (blocked_reason_comment.val().length === 0 || !blocked_reason_comment.val().trim()){
+      blocked_reason_comment.attr('style', 'border-color:red;')
+      return false;
+    }
+    if (!$('input[type=radio]:checked', '#blocked_reason_form').val()){
+      reason_label.attr('style', 'color:red;')
+      return false;
+    }
     return true;
   });
 
