b40d070c6a59f55a4edcc99ea84a46711a60eca5
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-11-03 18:12:20 +0200

    Refactored controller and js with a more readable implementation

--- app/controllers/blocked_reasons_controller.rb
+++ app/controllers/blocked_reasons_controller.rb
@@ -1,124 +1,117 @@
 class BlockedReasonsController < ApplicationController
+  before_filter :issue_is_editable, :find_current_blocked_reason
+  before_filter :find_blocked_reason_type, :create_new_blocked_reason_and_watcher,
+    only: [:create, :update]
+
   def create
-    issue = Issue.find(params[:blocked_reason][:issue_id])
-    redirect_to(issue, flash: { error: I18n.t('helpers.error.blocking_issue') }) and
-        return unless params[:blocked_reason][:blocked_reason_type] &&
-        params[:blocked_reason][:blocked_reason_type][:id]
-    if issue.editable?
-      blocked_reason_type = BlockedReasonType.where(
-        id: params[:blocked_reason][:blocked_reason_type][:id], removed: false).first
-      redirect_to(issue, flash: { error: I18n.t('helpers.error.blocking_issue') }) and
-        return unless blocked_reason_type
-      current_blocked_reason = BlockedReason.where(issue_id: issue.id, active: true).first
-      blocked_reason = BlockedReason.new comment: params[:blocked_reason][:comment],
-        blocked_reason_type_id: blocked_reason_type[:id], issue_id: issue[:id],
-        active: true, user_id: User.current.id, unblocker: false
-      watcher = Watcher.where(watchable_type: 'Issue', watchable_id: issue.id,
-        user_id: User.current.id
-        ).first || Watcher.new(watchable_type: 'Issue', watchable_id: issue.id,
-        user_id: User.current.id)
-      begin
-        Issue.transaction do
-          if current_blocked_reason
-            current_blocked_reason[:active] = false
-            current_blocked_reason.save!
-          end
-          watcher.save!
-          blocked_reason.save!
-          journal = issue.init_journal(User.current, "#{blocked_reason.comment}")
-          journal.details << JournalDetail.new(:property => 'attr',
-                                               :prop_key => 'blocked_status',
-                                               :value => I18n.t('blocked_reason'))
-          journal.details << JournalDetail.new(:property => 'attr',
-                                               :prop_key => 'blocked_reason',
-                                               :value => blocked_reason.type_name)
-          journal.save!
-          issue.touch
-          redirect_to issue, notice: I18n.t('helpers.success.blocked_issue')
-        end
-      rescue ActiveRecord::RecordInvalid => invalid
-        logger.error invalid.message
-        redirect_to issue, flash: { error: I18n.t('helpers.error.blocking_issue') }
+    saving_with_issue_transaction do
+      if @current_blocked_reason
+        @current_blocked_reason[:active] = false
+        @current_blocked_reason.save!
       end
-    else
-      redirect_to issue, flash: { error: I18n.t('helpers.error.blocked_permission_denied') }
+      @watcher.save!
+      @new_blocked_reason.save!
+      journal = @issue.init_journal(User.current, "#{@new_blocked_reason.comment}")
+      journal.details << JournalDetail.new(:property => 'attr',
+                                           :prop_key => 'blocked_status',
+                                           :value => I18n.t('blocked_reason'))
+      journal.details << JournalDetail.new(:property => 'attr',
+                                           :prop_key => 'blocked_reason',
+                                           :value => @new_blocked_reason.type_name)
+      journal.save!
+      @issue.touch
+      redirect_to @issue, notice: I18n.t('helpers.success.blocked_issue')
     end
   end
 
   def update
-    issue = Issue.find(params[:blocked_reason][:issue_id])
-    if issue.editable?
-      blocked_reason_type = BlockedReasonType.where(
-        id: params[:blocked_reason][:blocked_reason_type][:id], removed: false).first
-      redirect_to(issue, flash: { error: I18n.t('helpers.error.blocking_issue') }) and
-        return unless blocked_reason_type
-      current_blocked_reason = BlockedReason.where(issue_id: issue.id, active: true).first
-      blocked_reason = BlockedReason.new comment: params[:blocked_reason][:comment],
-        blocked_reason_type_id: blocked_reason_type[:id], issue_id: issue[:id],
-        active: true, user_id: User.current.id, unblocker: false
-      watcher = Watcher.where(watchable_type: 'Issue', watchable_id: issue.id,
-        user_id: User.current.id
-        ).first || Watcher.new(watchable_type: 'Issue', watchable_id: issue.id,
-        user_id: User.current.id)
-      begin
-        Issue.transaction do
-          current_blocked_reason[:active] = false
-          current_blocked_reason.save!
-          blocked_reason.save!
-          watcher.save!
-          journal = issue.init_journal(User.current, "#{blocked_reason.comment}")
-          if current_blocked_reason.blocked_reason_type_id == blocked_reason.blocked_reason_type_id
-            journal.details << JournalDetail.new(:property => 'attr',
-                                                 :prop_key => 'blocked_reason',
-                                                 :value => blocked_reason.type_name)
-          else
-            journal.details << JournalDetail.new(
-              :property => 'attr',
-              :prop_key => 'blocked_reason',
-              :old_value => current_blocked_reason.type_name,
-              :value => blocked_reason.type_name)
-          end
-          journal.save!
-          issue.touch
-          redirect_to issue, notice: I18n.t('helpers.success.blocked_issue')
-        end
-      rescue ActiveRecord::RecordInvalid => invalid
-        logger.error invalid.message
-        redirect_to issue, flash: { error: I18n.t('helpers.error.blocking_issue') }
+    saving_with_issue_transaction do
+      @current_blocked_reason[:active] = false
+      @current_blocked_reason.save!
+      @new_blocked_reason.save!
+      @watcher.save!
+      journal = @issue.init_journal(User.current, "#{@new_blocked_reason.comment}")
+      if @current_blocked_reason.blocked_reason_type_id == @new_blocked_reason.blocked_reason_type_id
+        journal.details << JournalDetail.new(:property => 'attr',
+                                             :prop_key => 'blocked_reason',
+                                             :value => @new_blocked_reason.type_name)
+      else
+        journal.details << JournalDetail.new(
+          :property => 'attr',
+          :prop_key => 'blocked_reason',
+          :old_value => @current_blocked_reason.type_name,
+          :value => @new_blocked_reason.type_name)
       end
-    else
-      redirect_to issue, flash: { error: I18n.t('helpers.error.blocked_permission_denied') }
+      journal.save!
+      @issue.touch
+      redirect_to @issue, notice: I18n.t('helpers.success.blocked_issue')
     end
   end
 
   def destroy
-    issue = Issue.find(params[:issue_id])
-    if issue.editable?
-      blocked_reason = BlockedReason.where(issue_id: issue.id, active: true).first
-      redirect_to issue, flash: { error: I18n.t('helpers.error.unblocking_issue') } and
-        return unless blocked_reason
-      blocked_reason[:active] = false
-      unblocked_reason = BlockedReason.new(
-        blocked_reason_type_id: blocked_reason[:blocked_reason_type_id], unblocker: true,
-        issue_id: issue[:id], active: false, user_id: User.current.id)
-      begin
-        Issue.transaction do
-          blocked_reason.save!
-          unblocked_reason.save!
-          journal = issue.init_journal(User.current, "#{blocked_reason.comment}")
-          journal.details << JournalDetail.new(:property => 'attr',
-                                               :prop_key => 'blocked_status',
-                                               :value => I18n.t('unblocked_reason'))
-          journal.save!
-          issue.touch
-          redirect_to issue, notice: I18n.t('helpers.success.unblocked_issue')
+    @current_blocked_reason[:active] = false
+    comment = params[:blocked_reason][:comment].blank? ? I18n.t('unblocked_reason') :
+      params[:blocked_reason][:comment]
+    unblocked_reason = BlockedReason.new(comment: comment, unblocker: true,
+      blocked_reason_type_id: @current_blocked_reason[:blocked_reason_type_id],
+      issue_id: @issue[:id], active: false, user_id: User.current.id)
+    saving_with_issue_transaction do
+      @current_blocked_reason.save!
+      unblocked_reason.save!
+      journal = @issue.init_journal(User.current, "#{unblocked_reason.comment}")
+      journal.details << JournalDetail.new(:property => 'attr',
+                                           :prop_key => 'blocked_status',
+                                           :value => I18n.t('unblocked_reason'))
+      journal.save!
+      @issue.touch
+      redirect_to @issue, notice: I18n.t('helpers.success.unblocked_issue')
+    end
+  end
+
+  private
+
+  def issue_is_editable
+    @issue = Issue.find(params[:blocked_reason][:issue_id])
+    redirect_to(@issue, flash: {
+      error: I18n.t('helpers.error.blocked_permission_denied')
+    }) and return unless @issue.editable?
+  end
+
+  def find_blocked_reason_type
+    @blocked_reason_type = BlockedReasonType.where(
+      id: params[:blocked_reason][:blocked_reason_type][:id], removed: false).first
+    redirect_to(@issue, flash: { error: I18n.t('helpers.error.blocking_issue') }) and
+      return unless @blocked_reason_type
+  end
+
+  def find_current_blocked_reason
+    @current_blocked_reason = BlockedReason.where(issue_id: @issue.id, active: true).first
+    redirect_to @issue, flash: { error: I18n.t('helpers.error.unblocking_issue') } and
+      return if @current_blocked_reason.nil? && params[:action] == :destroy
+  end
+
+  def create_new_blocked_reason_and_watcher
+    comment = params[:blocked_reason][:comment].blank? ? I18n.t('blocked_reason') :
+      params[:blocked_reason][:comment]
+    @new_blocked_reason = BlockedReason.new(comment: comment,
+      blocked_reason_type_id: @blocked_reason_type[:id], issue_id: @issue[:id],
+      active: true, user_id: User.current.id, unblocker: false)
+    @watcher = Watcher.where(watchable_type: 'Issue', watchable_id: @issue.id,
+      user_id: User.current.id).first_or_create
+  end
+
+  def saving_with_issue_transaction
+    begin
+      Issue.transaction do
+        begin
+          yield
+        rescue ActiveRecord::RecordInvalid => invalid
+          logger.error invalid.message
+          redirect_to @issue, flash: {
+            error: params[:action] == :destroy ? I18n.t('helpers.error.unblocking_issue') :
+              I18n.t('helpers.error.blocking_issue') }
         end
-      rescue ActiveRecord::RecordInvalid => invalid
-        logger.error invalid.message
-        redirect_to issue, flash: { error: I18n.t('helpers.error.unblocking_issue') }
       end
-    else
-      redirect_to issue, flash: { error: I18n.t('helpers.error.unblocked_permission_denied') }
     end
   end
 end
--- assets/javascripts/blocked_reason.js
+++ assets/javascripts/blocked_reason.js
@@ -9,10 +9,12 @@ $(document).ready(function() {
   var blocked_reason_window = $('#blocked_reason_window');
   var block_form_hide_button = $('#block_form_hide_button');
   var new_blocked_reason_button = $('#new_blocked_reason_button');
+  var delete_blocked_reason_button = $('#delete_unblocked_button');
   var blocked_reason_comment = $('#blocked_reason_comment');
   var reason_label = $('#reason_label');
 
   blocked_reason_comment.val('');
+
   block_issue_button.bind('click', function(event) {
     event.preventDefault();
     var position = block_issue_button.position();
@@ -24,27 +26,43 @@ $(document).ready(function() {
     });
     blocked_reason_window.show();
   });
+
   block_form_hide_button.bind('click', function() {
     blocked_reason_window.hide();
   });
+
   new_blocked_reason_button.bind('click', function(event) {
     event.preventDefault();
+    if (blocked_comment_completed() & blocked_reason_selected()) {
+      $('.blocked_reason_form').submit();
+    }
+  });
+
+  delete_blocked_reason_button.bind('click', function(event) {
+    event.preventDefault();
+    if (blocked_comment_completed()) {
+      $('#deleted_comment').val(blocked_reason_comment.val());
+      $('#delete_button_form').submit();
+    }
+  });
+
+  function blocked_comment_completed() {
     if (blocked_reason_comment.val().length === 0 || !blocked_reason_comment.val().trim()) {
       blocked_reason_comment.attr('style', 'border-color:red;');
       return false;
     }
-    else {
-      blocked_reason_comment.attr('style', 'border-color:#ccc;');
-    }
+    blocked_reason_comment.attr('style', 'border-color:#ccc;');
+    return true;
+  };
+
+  function blocked_reason_selected() {
     if (!$('input[type=radio]:checked', '#blocked_reason_window').val()) {
       reason_label.attr('style', 'color:red;');
       return false;
     }
-    else {
-      reason_label.attr('style', 'color:black;');
-    }
-    $('.blocked_reason_form').submit();
-  });
+    reason_label.attr('style', 'color:black;');
+    return true;
+  };
 
   $('.brt_tooltip').tipr({
     'speed': 0,
