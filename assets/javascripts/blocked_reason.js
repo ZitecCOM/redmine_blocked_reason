cb06ec795a3d21de664d23a9a1ad39460d3a46fc
unknown <cristianromantudor@yahoo.com>
2014-11-28 14:22:29 +0200

    Implemented design for blocked reason window

--- assets/javascripts/blocked_reason.js
+++ assets/javascripts/blocked_reason.js
@@ -13,18 +13,18 @@ $(document).ready(function() {
   var blocked_reason_comment = $('#blocked_reason_comment');
   var reason_label = $('#reason_label');
 
+  $('#blocked_reason_window .field, #block_issue_button').click(function() {
+    $('#blocked_reason_window #blocked_reason_comment').focus();
+  });
+
   blocked_reason_comment.val('');
 
   block_issue_button.bind('click', function(event) {
     event.preventDefault();
     var position = block_issue_button.position();
-    var pos_top = position.top;
-    var pos_left = position.left;
-    blocked_reason_window.css({
-      top: pos_top,
-      left: pos_left
-    });
     blocked_reason_window.show();
+    $('#blocked_reason_window .field:first > input[type="radio"]').prop("checked", true);
+    $('#blocked_reason_window #blocked_reason_comment').focus();
   });
 
   block_form_hide_button.bind('click', function() {
