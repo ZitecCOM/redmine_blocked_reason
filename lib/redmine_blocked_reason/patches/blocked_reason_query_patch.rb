fdefbd3b5fc9fa8183069753d168817ecd491fc7
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-01-16 17:29:14 +0200

    Fixed filter overwriting

--- lib/redmine_blocked_reason/patches/blocked_reason_query_patch.rb
+++ lib/redmine_blocked_reason/patches/blocked_reason_query_patch.rb
@@ -1,60 +1,70 @@
-module RedmineBlockedReason
-  module BlockedReasonQueryPatch
-    def self.included(base)
-      base.send(:include, InstanceMethods)
-      base.class_eval do
-        unloadable
-        alias_method_chain :available_filters, :blocked_reasons
-        alias_method_chain :sql_for_field, :blocked_reasons
-      end
-    end
-    module InstanceMethods
-      def available_filters_with_blocked_reasons
-        available_filters = available_filters_without_blocked_reasons
-        blocked_reason_filters = {
-          'blocked_reason' => {
-            :name => 'Blocked Reason',
-            :type => :list_optional,
-            :values => BlockedReasonType.where(removed: false).map {|b| [b.name, b.id.to_s] },
-            :order => available_filters.size + 1}
-        }
-        available_filters.merge!(blocked_reason_filters)
-      end
+require_dependency 'query'
+if ActiveSupport::Dependencies::search_for_file('issue_query')
+  require_dependency 'issue_query'
+end
 
-      def sql_for_field_with_blocked_reasons(field, operator, value, db_table, db_field, is_custom_filter=false)
-        if field == 'blocked_reason'
-          case operator
-          when '='
-            containment_type = 'IN'
-          when '!'
-            containment_type = 'NOT IN'
-          when '*'
-            containment_type = 'IN'
-          when '!*'
-            containment_type = 'NOT IN'
+module RedmineBlockedReason
+  module Patches
+    module BlockedReasonQueryPatch
+      def self.included(base)
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+          unloadable
+          alias_method_chain :sql_for_field, :blocked_reasons
+
+          alias_method :available_filters_without_blocked_reasons, :available_filters
+          alias_method :available_filters, :available_filters_with_blocked_reasons
+        end
+      end
+      module InstanceMethods
+        def available_filters_with_blocked_reasons
+          unless @available_filters
+            available_filters_without_blocked_reasons.merge!({
+              'blocked_reason' => {
+                :name => 'Blocked Reason',
+                :type => :list_optional,
+                :values => BlockedReasonType.where(removed: false).map {|b| [b.name, b.id.to_s] },
+                :order => 201}
+            })
           end
-          brt_ids = value.map {|v| v.to_i}.join(',')
-          sql = "issues.id #{containment_type} (
-              SELECT DISTINCT i.id FROM issues i
-                INNER JOIN blocked_reasons br ON br.issue_id = i.id
-                INNER JOIN blocked_reason_types brt ON br.blocked_reason_type_id = brt.id
-              WHERE br.active = true AND br.unblocker = false AND brt.removed = false"
-          if operator == '='
-            sql << " AND brt.id IN (#{brt_ids})"
-          elsif operator == '!'
-            sql << " AND brt.id IN (#{brt_ids})"
+          @available_filters
+        end
+
+        def sql_for_field_with_blocked_reasons(field, operator, value, db_table, db_field, is_custom_filter=false)
+          if field == 'blocked_reason'
+            case operator
+            when '='
+              containment_type = 'IN'
+            when '!'
+              containment_type = 'NOT IN'
+            when '*'
+              containment_type = 'IN'
+            when '!*'
+              containment_type = 'NOT IN'
+            end
+            brt_ids = value.map {|v| v.to_i}.join(',')
+            sql = "issues.id #{containment_type} (
+                SELECT DISTINCT i.id FROM issues i
+                  INNER JOIN blocked_reasons br ON br.issue_id = i.id
+                  INNER JOIN blocked_reason_types brt ON br.blocked_reason_type_id = brt.id
+                WHERE br.active = true AND br.unblocker = false AND brt.removed = false"
+            if operator == '='
+              sql << " AND brt.id IN (#{brt_ids})"
+            elsif operator == '!'
+              sql << " AND brt.id IN (#{brt_ids})"
+            end
+            sql << ')'
+            return sql
+          else
+            return sql_for_field_without_blocked_reasons(field, operator, value, db_table, db_field, is_custom_filter)
           end
-          sql << ')'
-          return sql
-        else
-          return sql_for_field_without_blocked_reasons(field, operator, value, db_table, db_field, is_custom_filter)
         end
       end
     end
   end
 end
 
-# Add module to Query
-unless Query.included_modules.include?(RedmineBlockedReason::BlockedReasonQueryPatch)
-  Query.send(:include, RedmineBlockedReason::BlockedReasonQueryPatch)
+base = ActiveSupport::Dependencies::search_for_file('issue_query') ? IssueQuery : Query
+unless base.included_modules.include?(RedmineBlockedReason::Patches::BlockedReasonQueryPatch)
+  base.send(:include, RedmineBlockedReason::Patches::BlockedReasonQueryPatch)
 end
