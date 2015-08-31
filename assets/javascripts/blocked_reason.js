884de7392f1ef24f959ff61c376f680ab08fab6c
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-08-31 16:30:18 +0300

    Progress on fixing window

--- assets/javascripts/blocked_reason.js
+++ assets/javascripts/blocked_reason.js
@@ -1,111 +1,123 @@
-$(document).on('click', function(event) {
-  if (!$(event.target).closest('#blocked_reason_window').length && !$(event.target).closest('#block_issue_button').length) {
-    $('#blocked_reason_window').hide();
-    var block_issue_button = $('#block_issue_button');
-    block_issue_button.unbind('click');
-    initialize_blocked_reason();
-  }
+'use strict';
+
+var BlockWindow = (function ($) {
+  var self = function (root) {
+    this.root = root;
+    this.button = this.root.find('.block-button');
+    this.modal = this.root.find('.block-modal');
+    this.initialize();
+  };
+
+  var def = self.prototype;
+
+  def.showModal = function () {
+    var commentText = this.modal.find('.comment-text');
+    this.modal.show();
+    commentText.focus();
+  };
+
+  def.addButtonClickEvents = function () {
+    this.button.on('click', function (event) {
+      event.preventDefault();
+      this.showModal();
+    }.bind(this));
+
+    $(document).on('click', function (event) {
+      var target = $(event.target);
+      var closestRoot = target.closest('span.block-reason');
+      if (!closestRoot.length || closestRoot[0] !== this.root[0]) {
+        this.modal.hide();
+      }
+    }.bind(this));
+  };
+
+  // def.addModalClickEvents = function () {
+
+  // };
+
+  def.initialize = function () {
+    this.addButtonClickEvents();
+    // this.addModalClickEvents();
+  };
+
+  return self;
+}($));
+
+$(function () {
+  $.map($('span.block-reason'), function (element) {
+    return new BlockWindow($(element));
+  });
+  $('.brt_tooltip').tipr({ 'speed': 0, 'mode': 'bottom' });
 });
 
-function initialize_blocked_reason(){
-  var block_issue_button = $('#block_issue_button');
-  var blocked_reason_window = $('#blocked_reason_window');
-  var block_form_hide_button = $('#block_form_hide_button');
-  var new_blocked_reason_button = $('#new_blocked_reason_button');
-  var delete_blocked_reason_button = $('#delete_unblocked_button');
-  var remove_blocked_reason_label = $('#removed_blocked_reason');
-  var blocked_reason_comment = $('#blocked_reason_comment');
-  var reason_label = $('#reason_label');
-  var radio_buttons = $('.blocked_reason_form .radio-buttons')
-  var blocked_title = $('.blocked-reason-window-title');
-  var comment_label = $('#comment_label');
+//   blockButton.on('click', show_window);
 
-  $('#blocked_reason_window .field, #block_issue_button').click(function() {
-    $('#blocked_reason_window #blocked_reason_comment').focus();
-    switch_buttons();
-  });
+//   function show_window(event) {
+//     event.preventDefault();
+//     blocked_reason_window.show();
+//     var remove_label_input = $('#blocked_reason_blocked_reason_type_id_remove');
+//     if (remove_label_input[0]) {
+//       remove_label_input.prop("checked", true);
+//       comment_label.css('display','none');
+//     }
+//     switch_buttons();
+//     $('.block-modal .blocked_reason_comment').focus();
+//     blockButton.bind('click', hide_window);
+//   };
 
-  blocked_reason_comment.val('');
+//   function hide_window(event) {
+//     event.preventDefault();
+//     blocked_reason_window.hide();
+//     blockButton.unbind('click');
+//     blockButton.bind('click', show_window);
+//   };
 
+//   block_form_hide_button.bind('click', function () {
+//     blocked_reason_window.hide();
+//     blockButton.bind('click', show_window);
+//   });
 
-  block_issue_button.bind('click', show_window);
+//   new_blocked_reason_button.bind('click', function (event) {
+//     event.preventDefault();
+//     if (blocked_comment_completed() && blocked_reason_label_selected()){
+//       $('.blocked_reason_form').submit();
+//     }
+//   });
 
-  function show_window(event) {
-    event.preventDefault();
-    blocked_reason_window.show();
-    var remove_label_input = $('#blocked_reason_blocked_reason_type_id_remove');
-    if (remove_label_input[0]) {
-      remove_label_input.prop("checked", true);
-      comment_label.css('display','none');
-    }
-    switch_buttons();
-    $('#blocked_reason_window #blocked_reason_comment').focus();
-    block_issue_button.bind('click', hide_window);
-  };
+//   delete_blocked_reason_button.bind('click', function (event) {
+//     event.preventDefault();
+//     $('#deleted_comment').val(blocked_reason_comment.val());
+//     $('#delete_button_form').submit();
+//   });
 
-  function hide_window(event) {
-    event.preventDefault();
-    blocked_reason_window.hide();
-    block_issue_button.unbind('click');
-    block_issue_button.bind('click', show_window);
-  };
+//   function blocked_comment_completed() {
+//     if (blocked_reason_comment.val().length === 0 || !blocked_reason_comment.val().trim()) {
+//       blocked_reason_comment.attr('style', 'border-color:red;');
+//       return false;
+//     }
+//     blocked_reason_comment.attr('style', 'border-color:#ccc;');
+//     return true;
+//   };
 
-  block_form_hide_button.bind('click', function() {
-    blocked_reason_window.hide();
-    block_issue_button.bind('click', show_window);
-  });
+//   function blocked_reason_label_selected() {
+//     if ($('input[type=radio]:checked')[0]) {
+//       blocked_title.attr('style', 'color:#272727;');
+//       return true;
+//     }
+//     blocked_title.attr('style', 'color:#f75c5c;');
+//     return false;
+//   };
 
-  new_blocked_reason_button.bind('click', function(event) {
-    event.preventDefault();
-    if (blocked_comment_completed() && blocked_reason_label_selected()){
-      $('.blocked_reason_form').submit();
-    }
-  });
-
-  delete_blocked_reason_button.bind('click', function(event) {
-    event.preventDefault();
-    $('#deleted_comment').val(blocked_reason_comment.val());
-    $('#delete_button_form').submit();
-  });
-
-  function blocked_comment_completed() {
-    if (blocked_reason_comment.val().length === 0 || !blocked_reason_comment.val().trim()) {
-      blocked_reason_comment.attr('style', 'border-color:red;');
-      return false;
-    }
-    blocked_reason_comment.attr('style', 'border-color:#ccc;');
-    return true;
-  };
-
-  function blocked_reason_label_selected(){
-    if ($('input[type=radio]:checked')[0]) {
-      blocked_title.attr('style', 'color:#272727;');
-      return true;
-    }
-    blocked_title.attr('style', 'color:#f75c5c;');
-    return false;
-  };
-
-  function switch_buttons() {
-    if ($('input[type=radio]:checked', '#blocked_reason_window').val() == 'remove'){
-      new_blocked_reason_button.hide();
-      remove_blocked_reason_label.show();
-      comment_label.css('display','none');
-    }
-    else{
-      new_blocked_reason_button.show();
-      remove_blocked_reason_label.hide();
-      comment_label.css('display','inline');
-    }
-  }
-
-};
-
-$(document).ready(function() {
-  initialize_blocked_reason();
-
-  $('.brt_tooltip').tipr({
-    'speed': 0,
-    'mode': 'bottom'
-  });
-});
+//   function switch_buttons() {
+//     if ($('input[type=radio]:checked', '.block-modal').val() == 'remove'){
+//       new_blocked_reason_button.hide();
+//       remove_blocked_reason_label.show();
+//       comment_label.css('display','none');
+//     }
+//     else{
+//       new_blocked_reason_button.show();
+//       remove_blocked_reason_label.hide();
+//       comment_label.css('display','inline');
+//     }
+//   }
+// };
