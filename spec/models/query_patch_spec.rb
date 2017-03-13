875806e1d8f658304579b394ecf05390b2d467ce
Marius BALTEANU <marius.balteanu@zitec.com>
2017-03-13 08:56:28 +0000

    fix failed test

--- spec/models/query_patch_spec.rb
+++ spec/models/query_patch_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Query, type: :model do
-  it 'is patched with RedmineBlockedReason::Patches::QueryPatch' do
-    patch = RedmineBlockedReason::Patches::QueryPatch
+  it 'is patched with RedmineBlockedReason::Patches::IssueQueryPatch' do
+    patch = RedmineBlockedReason::Patches::IssueQueryPatch
     expect(Query.included_modules).to include(patch)
   end
 end
